917f4453206d49f8265bf8cd120ba52d344724bf
Sudarshan Hosalli@DESKTOP-F068LQM <godhustler90956@gmail.com>
2022-07-09 10:39:12 +0530

    update

---
+++ src/Hero.js
@@ -0,0 +1,23 @@
+import group from './Group 77.png';
+
+
+export default  function Hero() {
+    return (
+      <>
+      
+      <div className='hero-grid'>
+    <div className='hero'>
+      <img src={group} className="group-img" alt="group-img" />
+  
+      </div>
+
+
+      <div className='text'>
+      <h1>Online Experiences</h1>
+<p>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
+      </div>
+      </div>
+      
+      </>
+    );
+  }
---
+++ src/Nav.js
@@ -0,0 +1,17 @@
+import airbnblogo from './airbnb 1.svg';
+
+
+export default  function Nav() {
+    return (
+      <>
+      
+      <nav>
+
+      <img src={airbnblogo} className="airbnb-logo" alt="logo" />
+
+      </nav>
+      
+      
+      </>
+    );
+  }
